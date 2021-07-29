b8029b41d5076ea895860a43f2f759b16ece81e0
Sebastian Mazur <71341665+Blejn@users.noreply.github.com>
2021-07-29 10:07:27 +0200

    Galactic Ball- all files upload

---
+++ Galactic Ball/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1176640.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 49684370, 49684376, 49684429, 49684434, 49688904, 49691808, 49695411, 49695733, 49695894, 49700821, 49700822, 49700838, 49700898, 49705715 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/49684326/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/49684327/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/49684325/1/ammo.js', 'preload' : true},
+];
